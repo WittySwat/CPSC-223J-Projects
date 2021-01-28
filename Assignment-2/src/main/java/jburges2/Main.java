380c5358615d67033c534239f712e2e41f4ca23c
WittySwat <wittyswat+github@gmail.com>
2021-01-28 09:24:35 -0800

    copyright

--- Assignment-2/src/main/java/jburges2/Main.java
+++ Assignment-2/src/main/java/jburges2/Main.java
@@ -1,11 +1,11 @@
 /*
- *  Program Name: "Payroll System".  This program shows how to add and subtract two numbers using a simple UI with three
- *  active buttons.  Copyright (C) 2021 Jarrod Burges
- *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
- *  version 3 as published by the Free Software Foundation.
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- *  A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.
+ * Program Name: "Payroll System".  This program shows how to add and subtract two numbers using a simple UI with three
+ * active buttons.  Copyright (C) 2021 Jarrod Burges
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.
  */
 package jburges2;
 
